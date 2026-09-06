bec564d095154795edc099815ea15f31be372e53
Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>
2026-09-06 21:51:43 +0530

    chore: add environment configuration

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+load_dotenv()
+
 app = FastAPI()
+
+API_KEY = os.getenv("API_KEY")
+TEST_TABLE = os.getenv("TEST_TABLE", "test_users")
 
 # TODO: add API authentication
 # TODO: add database connection
